fd3784cf2468ae46948bb0e0efceb010f90e0e24
Adil Aziz <adil.j13@gmail.com>
2019-05-29 14:10:05 +0500

    Fixed Facebook Comments 3

--- course.php
+++ course.php
@@ -76,7 +76,7 @@ if (in_array($slug,$courses)){
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
   js = d.createElement(s); js.id = id;
-  js.src = 'https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v3.1&appId=745296145630598&autoLogAppEvents=1';
+  js.src = '//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v3.1&appId=745296145630598&autoLogAppEvents=1';
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>
     <header role="banner">
